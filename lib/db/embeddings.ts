d79d294a463792e39df7938777eb7591d6848ee4
pranav kotwal <pkotwal24@gmail.com>
2024-04-27 20:36:27 +0530

    openai-edge attempt 1

--- lib/db/embeddings.ts
+++ lib/db/embeddings.ts
@@ -1,17 +1,18 @@
 import {OpenAIApi,Configuration} from 'openai-edge'
-
+// attempt 1 
 const config = new Configuration({
-    apiKey:process.env.OPENAI_API_KEY
+    apiKey:process.env.OPENAI_API_KEY!
 })
 const openai = new OpenAIApi(config)
 
 export async function getEmbeddings(text:string) {
     try {
         const response =  await openai.createEmbedding({
-            model: 'text-embedding-ada-002',
+            model: "text-embedding-3-small",
             input:text.replace(/\n/g,' ')
         })
         const result = await response.json()
+        console.log("Resssss",result)
 
         return result.data[0].embedding as number[]
     } catch (error) {
